0bbf0e7acfb9347c2d65f20dfe9fd4f1825fb81e
WHJ <weihj1145@gmail.com>
2023-10-06 19:28:32 +0800

    test

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import './assets/main.css'
-
+import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
 // 创建实例
+const pinia = createPinia()
 const app = createApp(App);//确保 _use_ 路由实例使整个应用支持路由。
 app.use(router);// 挂载router,完成
+app.use(pinia);
 app.mount('#app');
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,17 @@
 
 <template>
     <div>About</div>
-    
-    
+    <div>{{ useCounterStore.count }}</div>
 </template>
 <script>
-    
+    import {useCounterStore} from '@/stores/counter.js'
+    export default{
+        setup(){
+            const mainStore = useCounterStore();
+            console.log(mainStore.count);
+            return{
+                    mainStore
+                }    
+        }
+    }
 </script>
