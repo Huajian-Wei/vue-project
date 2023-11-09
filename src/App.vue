a6d3d612cf6c3fc143b3a7acfe6dbdc8df4fd520
WHJ <weihj1145@gmail.com>
2023-11-09 13:21:29 +0800

    logout

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
-<script>
 
-</script>
 
 <template>
   <div class="link">
@@ -8,13 +6,27 @@
     <RouterLink to="/about"><span class="font">关于|</span></RouterLink>
     <RouterLink to="/user/login"><span class="font">登录|</span></RouterLink>
     <RouterLink to="/user/register"><span class="font">注册|</span></RouterLink>
-    <RouterLink to="/user/index"><span class="font">用户中心</span></RouterLink>
+    <RouterLink to="/user/index"><span class="font">用户中心|</span></RouterLink>
+    <span class="font" @click="logout">退出登录</span>
     <div>
       <!-- 页面组件的占位 iframe -->
       <RouterView></RouterView>
     </div>
   </div>
 </template>
+<script>
+import { useUserStore } from '@/stores/user'
+export default{
+  methods: {
+        logout() {
+            const userStore = useUserStore();
+            console.log('aaa')
+            userStore.$reset();
+            this.$router.push('/user/login');
+        }
+    }
+}
+</script>
 <style>
 .link{
   margin-right: 20px;
